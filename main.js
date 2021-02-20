4fc15849421b62f7a02f898efca0ca4a091ca90e
Laufer4u <nzevgolis4u@gmail.com>
2021-02-20 21:30:48 +0200

    Created Class Circle

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-function addList(L) {
-    let s = 0;
-    for(let i=0; i<L.length; i++) {
-        s = s + L[i];
+
+class Circle {
+    constructor(x, y, r) { 
+        this.x = x;
+        this.y = y;
+        this.r = r;
     }
-    return s;
+    // Methods --> function inside class
+    getArea() {
+        return Math.PI*this.r**2;
+    }
+    getLength() {
+        return 2*Math.PI*this.r;
+    }
+    getCartesian() {
+        return '[(x - '+this.x+')^2 + (y - '+this.y+')^2 = '+this.r+'^2]';
+    }
+    getPoints(n) {
+        let data = [];
+        for (var i = 0; i<n; i++) {
+            let t = 2*(i/n)*Math.PI;
+            let x = this.r*Math.cos(t) + this.x;
+            let y = this.r*Math.sin(t) + this.y;
+            
+            data.push([x, y]); // add at end of list data
+        }
+        return data;
+    }
 }
-console.log(addList([1, 2, 3]));
+let circle = new Circle(0, 0, 1);
+console.log(circle.getPoints(20));
